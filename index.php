b7c46b85b6b250b0d416be180d599d495499a461
cerenkdy <128895754+cerenkdy@users.noreply.github.com>
2023-04-23 13:17:10 +0300

    index.php file has been created and implemented

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+    <link rel="stylesheet" href="/LandingPage/style.css">
+</head>
+<body>
+    <div class="main">
+        <div class="form-box">
+            
+            <!-- Home Automation title needs to be fixed -->            
+            <h2>Home Automation System</h2>
+            <div class="buttons nav">
+                <button class="button-link" onclick="window.location.href='/LandingPage/consumerLogin.php';">Consumer</button>
+                <button class="button-link button-producer" onclick="window.location.href='/LAndingPAge/producerLogin.php';">Producer</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
