a58dfc4ea31675fb9a05f40814b00f0fdb825bde
Mehmet BULUT <20200808008@ogr.akdeniz.edu.tr>
2023-06-04 12:34:06 +0300

    devices table

--- Database/database.sql
+++ Database/database.sql
@@ -24,3 +24,17 @@ CREATE TABLE rooms (
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+-- inserting rooms information
+
+CREATE TABLE devices (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    room_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL DEFAULT 'sensor',
+    status INT NOT NULL DEFAULT 0,
+    data text NOT NULL DEFAULT '{}',
+    PRIMARY KEY (id),
+    FOREIGN KEY (room_id) REFERENCES rooms(id)
+);
