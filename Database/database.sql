74d6780a22502fbb9ca9437899dc33a01d36ede7
Mehmet BULUT <20200808008@ogr.akdeniz.edu.tr>
2023-06-09 00:14:59 +0300

    device data and config data added

--- Database/database.sql
+++ Database/database.sql
@@ -44,6 +44,7 @@ INSERT INTO rooms (id, name, user_id, data) VALUES (2, 'Bedroom', 1, '{"temperat
 INSERT INTO rooms (id, name, user_id, data) VALUES (3, 'Kitchen', 1, '{"temperature": 25, "humidity": 55, "temperature_status":"1","humidity_status":"1"}');
 INSERT INTO rooms (id, name, user_id, data) VALUES (4, 'Bathroom', 1, '{"temperature": 23, "humidity": 65, "temperature_status":"1","humidity_status":"1"}');
 
+
 -- table for devices
 CREATE TABLE devices (
     id INT NOT NULL AUTO_INCREMENT,
@@ -59,45 +60,65 @@ CREATE TABLE devices (
     PRIMARY KEY (id)
 );
 
--- inserting the data of living room devices 
-INSERT INTO devices (id, name, room_id, type, status, data) VALUES (1, 'Sensor', '1', 0, '{temperature: 24, humidity: 50}');
-INSERT INTO devices (id, name, room_id, type, status, data) VALUES (2, 'Light', 0, '{color: #ffffff}');
-INSERT INTO devices (id, name, room_id, type, status, data) VALUES (3, 'Air Conditioner', 1, 'air_conditioner', 0, '{auto_start: 25}');
-INSERT INTO devices (id, name, room_id, type, status, data) VALUES (4, 'TV', 1, 'tv', 0, '{channel: 1, volume: 50}');
+-- devices data
+INSERT INTO devices (id, user_id, name, room_id, type, status, data, electricity) VALUES (2, 1, 'Main Lamp', 1, 'light', 0, '{"color": "#ffffff"}', 1);
+INSERT INTO devices (id, user_id, name, room_id, type, status, data, electricity) VALUES (3, 1, 'Bedroom Lamp', 2, 'light', 0, '{"color": "#ffffff"}', 1);
+INSERT INTO devices (id, user_id, name, room_id, type, status, data, electricity) VALUES (4, 1, 'Kitchen Lamp', 3, 'light', 0, '{"color": "#ffffff"}', 1);
+INSERT INTO devices (id, user_id, name, room_id, type, status, data, electricity) VALUES (5, 1, 'Bathroom Lamp', 4, 'light', 0, '{"color": "#ffffff"}', 1);
+INSERT INTO devices (id, user_id, name, room_id, type, status, data, electricity) VALUES (6, 1, 'Air Conditioner', 1, 'ac', 0, '{"auto_start": 25}', 1);
+INSERT INTO devices (id, user_id, name, room_id, type, status, data, electricity) VALUES (7, 1, 'TV', 1, 'tv', 1, '{"channel": 1, "volume": 50}', 1);
+
+-- home configs table
+CREATE TABLE home_configs (
+    id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    data text NOT NULL DEFAULT '',
+    PRIMARY KEY (id)
+);
+
+-- home configs data
+INSERT INTO home_configs (id, user_id, type, data) VALUES (1, 1, 'outdoor_lock', '{"status": 0}');
+INSERT INTO home_configs (id, user_id, type, data) VALUES (2, 1, 'door', '{"name": "Main Door", "status": 1}');
+INSERT INTO home_configs (id, user_id, type, data) VALUES (3, 1, 'door', '{"name": "Exit Door", "status": 1}');
+INSERT INTO home_configs (id, user_id, type, data) VALUES (4, 1, 'door', '{"name": "Balcony Door", "status": 1}');
+INSERT INTO home_configs (id, user_id, type, data) VALUES (5, 1, 'door', '{"name": "Garage Door", "status": 1}');
+
 
 -- table for sensor data 
 CREATE TABLE sensor_data (
     id INT NOT NULL AUTO_INCREMENT,
-    user_id INT NOT NULL AUTO_INCREMENT,
-    device_id INT NOT NULL,
-    temperature decimal(2,1) NOT NULL,
-    humidity INT NOT NULL,
+    user_id INT NOT NULL,
+    room_id INT NOT NULL,
+    temperature INT NOT NULL DEFAULT 0,
+    humidity INT NOT NULL DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
-    FOREIGN KEY (device_id) REFERENCES devices(id)
+    FOREIGN KEY (room_id) REFERENCES rooms(id)
 );
 
 -- mock sensor data for demo
-INSERT INTO sensor_data (device_id, temperature,humidity) VALUES (1, 24, 50);
-INSERT INTO sensor_data (device_id, temperature,humidity) VALUES (1, 25, 51);
-INSERT INTO sensor_data (device_id, temperature,humidity) VALUES (1, 22, 50);
-INSERT INTO sensor_data (device_id, temperature,humidity) VALUES (1, 24, 49);
-INSERT INTO sensor_data (device_id, temperature,humidity) VALUES (1, 25, 50);
-INSERT INTO sensor_data (device_id, temperature,humidity) VALUES (1, 24, 50);
+INSERT INTO sensor_data (user_id, room_id, temperature, humidity) VALUES (1, 1, 24, 50);
+INSERT INTO sensor_data (user_id, room_id, temperature, humidity) VALUES (1, 1, 25, 51);
+INSERT INTO sensor_data (user_id, room_id, temperature, humidity) VALUES (1, 1, 22, 50);
+INSERT INTO sensor_data (user_id, room_id, temperature, humidity) VALUES (1, 1, 24, 49);
+INSERT INTO sensor_data (user_id, room_id, temperature, humidity) VALUES (1, 1, 25, 50);
+INSERT INTO sensor_data (user_id, room_id, temperature, humidity) VALUES (1, 1, 24, 50);
+INSERT INTO sensor_data (user_id, room_id, temperature, humidity) VALUES (1, 1, 24, 51);
 
--- table for logs   
+-- table for logs 
 CREATE TABLE logs (
     id INT NOT NULL AUTO_INCREMENT,
     user_id INT NOT NULL,
     user_type VARCHAR(255) NOT NULL DEFAULT 'consumers',
     device_id INT NOT NULL,
-    action VARCHAR(255) NOT NULL,
+    action INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
-    FOREIGN KEY (device_id) REFERENCES devices (id)
+    FOREIGN KEY (device_id) REFERENCES devices(id)
 );
 
 -- mock log data for demo
-INSERT INTO logs (user_id, user_type, device_id, action) VALUES (1, 'consumers', 1, 'Smart TV turned on');
-INSERT INTO logs (user_id, user_type, device_id, action) VALUES (1, 'consumers', 3, 'Air Conditioner turned off');
-INSERT INTO logs (user_id, user_type, device_id, action) VALUES (1, 'consumers', 2, 'Lamps turned off');
+INSERT INTO logs (user_id, user_type, device_id, action) VALUES (1, 'consumers', 1, 1);
+INSERT INTO logs (user_id, user_type, device_id, action) VALUES (1, 'consumers', 3, 0);
+INSERT INTO logs (user_id, user_type, device_id, action) VALUES (1, 'consumers', 2, 0);
